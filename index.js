2299989cce6339ef833d7f2859126a0fba02ed7f
Theo Weller <weller.theodor@gmail.com>
2019-04-20 01:00:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+const companyName = 'Scuber';// Code your solution in this file!
+
+const mostProfitableNeighborhood = 'Chelsea';
+
+const companyCeo = 'Susan Smith';
